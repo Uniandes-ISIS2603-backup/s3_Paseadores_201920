427459318505c6e1fb8dcb3613422fa4dfa0c602
juan vergara <je.vergara@uniandes.edu.co>
2019-08-28 13:30:23 -0500

    ...

--- s3_paseadores-back/src/main/java/co/edu/uniandes/csw/paseadores/persistence/TarjetaCreditoPersistence.java
+++ s3_paseadores-back/src/main/java/co/edu/uniandes/csw/paseadores/persistence/TarjetaCreditoPersistence.java
@@ -6,9 +6,11 @@
 package co.edu.uniandes.csw.paseadores.persistence;
 
 import co.edu.uniandes.csw.paseadores.entities.TarjetaCreditoEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -23,5 +25,21 @@ public class TarjetaCreditoPersistence {
     public TarjetaCreditoEntity create(TarjetaCreditoEntity p){
         em.persist(p);
         return p;
+    }
+    public TarjetaCreditoEntity find (Long idTarjetaCredito){
+         return em.find(TarjetaCreditoEntity.class, idTarjetaCredito);
+    }
+    public List<TarjetaCreditoEntity> findAll(){
+        TypedQuery query = em.createQuery("selecciona u de tarjetaCreditoEntity u", TarjetaCreditoEntity.class);
+        return query.getResultList();
+    }
+    
+    public TarjetaCreditoEntity update (TarjetaCreditoEntity tarjetaCreditoEntity){
+        return em.merge(tarjetaCreditoEntity);
+    }
+    
+    public void delete(Long tarjetaCreditoId){
+        TarjetaCreditoEntity authorEntity = em.find(TarjetaCreditoEntity.class, tarjetaCreditoId);
+        em.remove(authorEntity);
     }
 }
