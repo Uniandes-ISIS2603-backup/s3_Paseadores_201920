b547c63e12818cd1c4e700423e244386b2c95803
Daniel Felipe Garcia Vargas <df.garcia@uniandes.edu.co>
2019-11-11 22:01:16 -0500

    Prueba Postman

--- s3_paseadores-api/src/test/java/co/edu/uniandes/csw/paseadores/tests/postman/CalificacionIT.java
+++ s3_paseadores-api/src/test/java/co/edu/uniandes/csw/paseadores/tests/postman/CalificacionIT.java
@@ -34,7 +34,7 @@ public class CalificacionIT {
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "s3_paseadores-api-1.0.war")//War del modulo api
+        return ShrinkWrap.create(WebArchive.class, "s3_paseadores-api.war")//War del modulo api
                 // Se agrega las dependencias
                 .addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
                         .importRuntimeDependencies().resolve()
@@ -56,7 +56,7 @@ public class CalificacionIT {
     @RunAsClient
     public void postman() throws IOException {
         PostmanTestBuilder tp = new PostmanTestBuilder();
-        tp.setTestWithoutLogin(COLLECTION, "Entorno-Colecciones.postman_environment");
+        tp.setTestWithoutLogin(COLLECTION, "Entorno-IT.postman_environment");
         String desiredResult = "0";
         Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult, tp.getIterations_failed());
 
